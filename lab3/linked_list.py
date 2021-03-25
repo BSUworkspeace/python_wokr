10088f1bb38148f6f81f700b8c0e74ff33f39ea7
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-03-25 09:18:01 +0300

    add

--- lab3/linked_list.py
+++ lab3/linked_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
 @author: soliva
@@ -8,45 +7,49 @@
 @desc:
 '''
 
-class Node ( object ):
-    def __init__ ( self , value , next_ = None ):
+
+class Node (object):
+    def __init__(self, value, next_=None):
         self._value = value
         self._next = next_
 
-        assert isinstance(self._value, int ) or isinstance(self._value, Node )
-        assert isinstance(self._next, Node ) or (self.next_ == None)
+        assert isinstance(self._value, int) or isinstance(self._value, Node)
+        assert isinstance(self._next, Node) or (self.next_ == None)
 
     @property
     def value(self):
         return self._value
+
     @property
-    def next_( self):
+    def next_(self):
         return self._next
 
+    def __repr__(self):
+        return str(self.value)
+
     def __iter__(self):
-        print("the value is:",self.value,"             the next value is:",self._next)
-        try:
+        print("the value is:", self.value,
+              "             the next value is:", self._next)
+        while self.next_ is not None:
             return iter(self._next)
-        except:
-            return None
 
+    def flatten_linked_list(self, node):
+        while node is not None:
+            yield node
+            node = node.next
 
-
-def flatten_linked_list(node):
-    while 1:
-        try:
-            x = iter(node)
-            x.value
-        except TypeError:
-            break
     # print(node.value,node.next_)
+
+
 if __name__ == '__main__':
 
     # r1 = Node ( 1 ) # 1 -> None - just one node
-    # r2 = Node (7 , Node (2 , Node ( 9 ) ) ) # 7 -> 2 -> 9 -> None
+    r2 = Node(7, Node(2, Node(9)))  # 7 -> 2 -> 9 -> None
+    for i in r2:
+        print(i)
     # # # 3 -> (19 -> 25 -> None ) -> 12 -> None
-    r3 = Node(3,Node(Node(19,Node(25)),Node(12)))
-    r3_flattenned = flatten_linked_list(r3) # 3 -> 19 -> 25 -> 12 -> None
-    r3_expected_flattenned_collection = [3 , 19 , 25 , 12]
-    # print(list( r3_flattenned ))
-    assert r3_expected_flattenned_collection == list( r3_flattenned )
+    # r3 = Node(3, Node(Node(19, Node(25)), Node(12)))
+    # r3_flattenned = Node.flatten_linked_list(r3)  # 3 -> 19 -> 25 -> 12 -> None
+    # r3_expected_flattenned_collection = [3, 19, 25, 12]
+    # # print(list( r3_flattenned ))
+    # assert r3_expected_flattenned_collection == list(r3_flattenned)
