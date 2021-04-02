52bd0df2ff2904e4cf8361e812c3e4d30b5971fd
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-04-02 19:41:32 +0300

    add

--- lab3/linked_list.py
+++ lab3/linked_list.py
@@ -13,8 +13,13 @@ class Node (object):
         self._value = value
         self._next = next_
 
+
+class flatten_linked_list():
+    def __init__(self):
         assert isinstance(self._value, int) or isinstance(self._value, Node)
         assert isinstance(self._next, Node) or (self.next_ == None)
+        if self.next_ == None:
+            pass
 
     @property
     def value(self):
@@ -24,19 +29,20 @@ class Node (object):
     def next_(self):
         return self._next
 
-    def __repr__(self):
-        return str(self.value)
+    # def __repr__(self):
+    #     return str(self.value)
 
-    def __iter__(self):
-        print("the value is:", self.value,
-              "             the next value is:", self._next)
-        while self.next_ is not None:
-            return iter(self._next)
+    # def __iter__(self):
+    #     print("the value is:", self.value,
+    #           "             the next value is:", self._next)
+    #     while self._next is not None:
+    #         print(self._next == None)
+    #         return iter(self._next)
 
-    def flatten_linked_list(self, node):
-        while node is not None:
-            yield node
-            node = node.next
+    # def flatten_linked_list(self, node):
+    #     while node is not None:
+    #         yield node
+    #         node = node.next
 
     # print(node.value,node.next_)
 
@@ -45,8 +51,8 @@ if __name__ == '__main__':
 
     # r1 = Node ( 1 ) # 1 -> None - just one node
     r2 = Node(7, Node(2, Node(9)))  # 7 -> 2 -> 9 -> None
-    for i in r2:
-        print(i)
+    # for i in r2:
+    #     print(i)
     # # # 3 -> (19 -> 25 -> None ) -> 12 -> None
     # r3 = Node(3, Node(Node(19, Node(25)), Node(12)))
     # r3_flattenned = Node.flatten_linked_list(r3)  # 3 -> 19 -> 25 -> 12 -> None
